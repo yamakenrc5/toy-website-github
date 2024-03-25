3f27fbbecedf2caca2f1e3d8f3b1b37a5890adc0
Kenji Yamamoto <kenji.yamamoto1@nnlife.co.jp>
2024-03-26 00:58:02 +0900

    Configure HTTPS on website at 2024-3月-26, 00:58:02

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@
 # file name, version, description, usage, design tips
 # params and vars
 $datetime = (Get-Date -Format 'yyyy-MMM-dd, HH:mm:ss')
-$msgbody = 'Fix string interpolation'
+$msgbody = 'Configure HTTPS on website'
 $commitmessage = "$msgbody at $datetime"
 $addtorepository = '.' #'deploy/azure-pipelines.yml'
 #command body
